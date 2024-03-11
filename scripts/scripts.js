07216f916a796801606179bfeec92bc3501e0f09
AndoniRoque <roque.andoni@gmail.com>
2024-03-11 13:58:56 -0300

    player construct

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,3 +3,25 @@ function createaGameboard () {
 }
 
 console.log(createaGameboard());
+
+function Player(mark, name) {
+    this.mark = mark;
+    this.name = name;
+}
+
+const xPlayer = new Player("x", "equis");
+
+const oPlayer = new Player("o", "circulo");
+
+// console.log(xPlayer);
+// console.log(oPlayer);
+
+function turn(player){
+    return player.mark == "x" ? oPlayer : xPlayer;
+}
+
+console.log(turn(oPlayer));
+
+function winState(gameboard) {
+    // ??? 
+}
