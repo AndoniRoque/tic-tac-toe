0ea4441f766661c6e29b41ddc80b979dfeda5f9a
AndoniRoque <roque.andoni@gmail.com>
2024-03-11 10:44:00 -0300

    second commit

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -10,3 +10,52 @@ function makeAdding(firstNumber) {
 const add5 = makeAdding(5);
 
 console.log(add5(2));
+
+const name = "Bob";
+const age = 28;
+const color = "red";
+
+const thatObject = {name: name, age: age, color:color};
+
+console.log(thatObject);
+
+const otherObject = {name, age, color};
+
+console.log(">", otherObject);
+
+console.log("-------------------------------------------");
+
+const obj = {a: 1, b:2};
+console.log(obj);
+const {a,b} = obj;
+console.log(obj);
+
+console.log("-------------------------------------------");
+
+function createUser(name){
+    const discordName = "@" + name;
+
+    let reputation = 0;
+    const getReptutation = () => reputation;
+    const giveReputation = () => reputation++;
+
+    return {name, discordName, getReptutation, giveReputation};
+}
+
+const josh = createUser("josh");
+josh.giveReputation();
+josh.giveReputation();
+
+console.log({
+    discordName: josh.discordName,
+    reputation: josh.getReptutation()
+})
+
+console.log("-------------------------------------------");
+
+function createPlayer (name, level) {
+    const { getReputation, giveReputation } = createUser(name);
+  
+    const increaseLevel = () => level++;
+    return { name, getReputation, giveReputation, increaseLevel };
+}
