d6f1faab779e9e1ddd66fc3879e17117d300fb85
Andoni Roque <roque.andoni@gmail.com>
2024-03-12 18:49:48 -0300

    what

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,9 +1,9 @@
+let gameboard = createGameboard();
+
+
 function createGameboard () {
     const gameboard = [["","",""], ["","",""], ["","",""]];
-
     const cells = document.querySelectorAll('.gameboard div');
-
-    
     return {gameboard, cells};
 }
 
@@ -12,19 +12,13 @@ function Player(mark, name) {
     this.name = name;
 }
 
-function turn(player){
-    return player.mark == "x" ? oPlayer : xPlayer;
-}
-
 function placeMarker(player, row, col) {
     const marker = player.mark;
-    gameboard[row][col] = (marker);
-    return gameboard; 
+    gameboard.gameboard[row][col] = marker;
 }
 
 function game() {
-    let round = 0;
-    
+    let round = 0;    
     const xPlayer = new Player("x", "equis");
     const oPlayer = new Player("o", "circulo");
     
@@ -32,15 +26,14 @@ function game() {
     for(let j = 0; j < 3; j++){
         for(let t = 0; t < 3; t++){
             
-            round++;
-            
-            // let rowInput = prompt("inserte fila donde quiere jugar");
-            // let colInput = prompt("inserte columna donde quiere jugar");
-            
+            round++;     
+                
             if(round % 2 == 0){
-                console.log("placeMarker", placeMarker(oPlayer, rowInput, colInput));
+                userInput(gameboard.cells, oPlayer);                
+                // console.log("placeMarker", placeMarker(oPlayer, rowInput, colInput));
             } else {
-                console.log("placeMarker", placeMarker(xPlayer, rowInput, colInput));
+                userInput(gameboard.cells, xPlayer);
+                // console.log("placeMarker", placeMarker(xPlayer, rowInput, colInput));
             }
             
             if(winState()) {
@@ -49,49 +42,51 @@ function game() {
         }
     }
     
-    alert("It's a tie!");
-    return;
+    if(round == 9) {
+        alert("It's a tie!");
+        return;
+    }
+        
 }
 
 
 function winState(){
     let won = false;
     for (let i = 0; i < 3; i++) {
-        if (gameboard[i][0] === gameboard[i][1] && gameboard[i][1] === gameboard[i][2] && gameboard[i][0] !== "") {
+        if (gameboard.gameboard[i][0] === gameboard.gameboard[i][1] && gameboard.gameboard[i][1] === gameboard.gameboard[i][2] && gameboard.gameboard[i][0] !== "") {
             won = true;
-            alert("Player " + gameboard[i][0] + " won!");
+            alert("Player " + gameboard.gameboard[i][0] + " won!");
             return true;
         }
     }
     
     for (let i = 0; i < 3; i++) {
-        if (gameboard[0][i] === gameboard[1][i] && gameboard[1][i] === gameboard[2][i] && gameboard[0][i] !== "") {
+        if (gameboard.gameboard[0][i] === gameboard.gameboard[1][i] && gameboard.gameboard[1][i] === gameboard.gameboard[2][i] && gameboard.gameboard[0][i] !== "") {
             won = true;
-            alert("Player " + gameboard[0][i] + " won!");
+            alert("Player " + gameboard.gameboard[0][i] + " won!");
             return true;
         }
     }
     
-    if ((gameboard[0][0] === gameboard[1][1] && gameboard[1][1] === gameboard[2][2] && gameboard[0][0] !== "") ||
-    (gameboard[0][2] === gameboard[1][1] && gameboard[1][1] === gameboard[2][0] && gameboard[0][2] !== "")) {
+    if ((gameboard.gameboard[0][0] === gameboard.gameboard[1][1] && gameboard.gameboard[1][1] === gameboard.gameboard[2][2] && gameboard.gameboard[0][0] !== "") ||
+    (gameboard.gameboard[0][2] === gameboard.gameboard[1][1] && gameboard.gameboard[1][1] === gameboard.gameboard[2][0] && gameboard.gameboard[0][2] !== "")) {
         won = true;
-        alert("Player " + gameboard[1][1] + " won!");
+        alert("Player " + gameboard.gameboard[1][1] + " won!");
         return true;
     }   
 }
 
 
-function userInput(cells){
+function userInput(cells, player){
+    console.log(player);
     cells.forEach(cell => {
         cell.addEventListener('click', () =>{
-            console.log("click on", cell.id);
+            cell.textContent = player.mark;
+            return cell;
         })
     });
 
 }
 
-
-let gameboard = createGameboard();
-userInput(createGameboard().cells);
 
 game();
