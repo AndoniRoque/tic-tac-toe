8c2fc18e2caa3f78b312a4b82302a6923f3a76ff
AndoniRoque <roque.andoni@gmail.com>
2024-03-12 10:37:52 -0300

    fill gameboard

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,8 +1,10 @@
 function createaGameboard () {
-    return new Array(9);
+    const gameboard = [["", "",""], ["","",""], ["","",""]]
+    return gameboard;
 }
 
-console.log(createaGameboard());
+let gameboard = createaGameboard();
+console.log("Tablero", gameboard);
 
 function Player(mark, name) {
     this.mark = mark;
@@ -13,15 +15,48 @@ const xPlayer = new Player("x", "equis");
 
 const oPlayer = new Player("o", "circulo");
 
-// console.log(xPlayer);
-// console.log(oPlayer);
+console.log("Jugador X: ", xPlayer);
+console.log("Jugador 0: ", oPlayer);
 
 function turn(player){
     return player.mark == "x" ? oPlayer : xPlayer;
 }
 
-console.log(turn(oPlayer));
-
-function winState(gameboard) {
-    // ??? 
+function placeMarker(player, row, col) {
+    const marker = player.mark;
+    gameboard[row][col] = (marker);
+    return gameboard; 
 }
+
+let round = 0;
+
+function game() {
+    for(let j = 0; j < 3; j++){
+        for(let t = 0; t < 3; t++){
+            round++;
+            let rowInput = prompt("inserte fila donde quiere jugar");
+            let colInput = prompt("inserte columna donde quiere jugar");
+            if(round % 2 == 0){
+                console.log("placeMarker", placeMarker(oPlayer, rowInput, colInput));
+            } else {
+                console.log("placeMarker", placeMarker(xPlayer, rowInput, colInput));
+            }
+            // winState();
+        }
+    }
+}
+
+// function winState(){
+//     let won = false;
+//     gameboard.forEach(row => {
+//         if(row[0] == "x" && row[1] == "x" && row[2] == "x"){
+//             won = true;
+//             alert("won!")
+//         }        
+//     });
+// }
+
+
+
+
+    
